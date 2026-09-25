5ef0c513191694c6aa1f16f20e0f86e2f4422b4c
Srihari <99220041874@klu.ac.in>
2026-09-25 12:05:58 +0530

    created api for the restaurant

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 
 import authRouter from "./routes/auth.routes.js";
+import restaurantRouter from "./routes/restaurantRoutes.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
 app.use("/api/auth",authRouter)
+app.use("/api/restaurants",restaurantRouter)
 
 //Global error handler
 app.use((err:Error,req:Request,res:Response,next:NextFunction)=>{
